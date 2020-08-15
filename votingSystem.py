e81f85f62268ca1a501b6a39eb156e85b4f3b857
Sahar Kausar <saharkausar@gmail.com>
2020-08-15 00:39:14 -0700

    Adding votingSystem.py
    [Initial Code Push] A Python-Based Voting System Project that allows the user to login with their registered voter ID, cast their vote, and view the election results. The code also imports modules from PySimpleGUI API.

---
+++ votingSystem.py
@@ -0,0 +1,218 @@
+#   SCRIPT: votingSystem.py
+#   AUTHOR: Sahar Kausar
+#   CONTACT: saharkausar@gmail.com
+#
+#   Anita Borg - Python Certification Course
+#
+#   DESCRIPTION: A Python-Based (Bonus and Final) Project Through the Anita Borg - Python Certification Course!
+#                The objective of the program is to provide a national voting system for the citizens of a country.
+
+import PySimpleGUI as sg
+
+##-----BUTTON SETTINGS----------------------------------##
+layout1_button_spec: dict = {'size':(18, 1), 'font':('Franklin Gothic Book', 22), 'button_color':("White","#bdd3de")}
+layout2_button_spec: dict = {'size':(13, 1), 'font':('Franklin Gothic Book', 12), 'button_color':("White","#bdd3de")}
+
+##-----VARIOUS LAYOUTS---------------------------------##
+layout1: list = [
+    [sg.Text('National Voting System', size=(50,1), justification='center', background_color="#debdc2",
+        text_color='White', font=('Franklin Gothic Book', 14, 'bold'))],
+    [sg.Button('Login to Cast Vote', **layout1_button_spec)],
+    [sg.Button('See Voting Results', **layout1_button_spec)],
+    [sg.Button('Exit Program', **layout1_button_spec),]
+]
+
+layout2 = [
+    [sg.Text('Example Name and Voter ID Input (First Name and Integer Number): Bob101')],
+    [sg.Text('Enter Your Name and Voter ID:'), sg.InputText()],
+    [sg.Button('Enter Voter ID', **layout2_button_spec), sg.Button('Main Menu', **layout2_button_spec)]
+]
+
+
+layout3 = [
+    [sg.Text('Hint - The Password is: VoteNow')],
+    [sg.Text('Please enter the password:'), sg.InputText()],
+    [sg.Button('Enter Password', **layout2_button_spec), sg.Button('Back', **layout2_button_spec)]
+]
+
+layout4 = [
+    [sg.Text('Please Vote for a Party:')],
+    [sg.Button('Believers', **layout2_button_spec), sg.Button('Dreamers', **layout2_button_spec)],
+]
+
+##-----WINDOW AND LAYOUT---------------------------------##
+layout = [[sg.Column(layout1, key='-COL1-', element_justification='center'), sg.Column(layout2, visible=False, key='-COL2-', element_justification='center'), sg.Column(layout3, visible=False, key='-COL3-', element_justification='center'), sg.Column(layout4, visible=False, key='-COL4-', element_justification='center')]]
+window: object = sg.Window('National Voting System', layout=layout, background_color="#debdc2", size=(500, 280), element_justification='center')
+
+##-----HELPER FUNCTIONS BASED ON CLICK EVENTS---------------------------------##
+
+#Defines the initial voting count for the election parties
+Believers = 0
+Dreamers  = 0
+
+#Function that allows a valid user to vote!
+def voteNow():
+
+    #Launches the voting layout
+    global layout
+    layout = 2
+    window[f'-COL{layout}-'].update(visible=False)
+    layout = layout + 2
+    window[f'-COL{layout}-'].update(visible=True)
+
+    while True:
+        event, values = window.read()
+
+        #If the user selects the 'Believers' party and votes for them
+        if event == 'Believers':
+            #Then we increment the vote count of that party by 1
+            global Believers
+            Believers = Believers + 1
+
+            #Displays the prompt that the vote was registered
+            print("Your vote for the Believers party has been registered. Thank you for voting!")
+            print(Believers)
+
+            #Returns the user to the main menu
+            window[f'-COL{layout}-'].update(visible=False)
+            layout = layout - 3
+            window[f'-COL{layout}-'].update(visible=True)
+            return Believers
+            break
+
+        #If the user selects the 'Dreamers' party and votes for them
+        elif event == 'Dreamers':
+            #Then we increment the vote count of that party by 1
+            global Dreamers
+            Dreamers = Dreamers + 1
+
+            #Displays the prompt that the vote was registered
+            print("Your vote for the Dreamers party has been registered. Thank you for voting!")
+            print(Dreamers)
+
+            #Returns the user to the main menu
+            window[f'-COL{layout}-'].update(visible=False)
+            layout = layout - 3
+            window[f'-COL{layout}-'].update(visible=True)
+            return Dreamers
+            break
+
+#Class that creates objects of each designated voter
+class Voter:
+  def __init__(self, name, voted):
+    self.name = name
+    self.voted = voted
+
+v1 = Voter("Bob101", False)
+v2 = Voter("Sally102", False)
+v3 = Voter("Joe103", False)
+
+#Function that checks whether the user's voter ID is valid or invalid
+def checkVoterID(voterID):
+
+    #List of individuals that are registered (valid voters)
+    validVoters = [v1, v2, v3]
+    validVoters_name = [v1.name, v2.name, v3.name]
+    validVoters_voted = [v1.voted, v2.voted, v3.voted]
+
+    #If the input given by the user is not registered in our validVoters list
+    if voterID not in validVoters_name:
+        #Then we return a message listing it as invalid credentials and redirects the user to the initial page menu.
+        global layout, Column
+        layout = 2
+        window[f'-COL{layout}-'].update(visible=False)
+        layout = layout - 1
+        window[f'-COL{layout}-'].update(visible=True)
+
+        print("Invalid Voter ID! Please return to the main menu and enter a registered voter ID!")
+
+    #If the input given by the user is a voter registered in our validVoters list
+    elif voterID in validVoters_name:
+        #Then we know they are a valid registered user - Let's check if they have voted or not!
+
+        #If the result of the voter is True (the voter has already voted)
+        if True in validVoters_voted:
+            #Then we let the user know that they have voted and redirect them to the main menu
+            print("You have already voted - Thank you! Please return to the main menu!")
+
+            window[f'-COL{layout}-'].update(visible=False)
+            layout = layout - 1
+            window[f'-COL{layout}-'].update(visible=True)
+
+        #If the result is False, then we know they have yet to vote - Let's vote!
+        elif False in validVoters_voted:
+
+            #Bob101
+            if voterID == validVoters_name[0]:
+                #Then we set Bob's voted value to True (Bob has voted)
+                v1.voted = True
+
+                #Redirects the user to the voting page to vote!
+                voteNow()
+
+            #Sally102
+            elif voterID == validVoters_name[1]:
+                #Then we set Sally's voted value to True (Bob has voted)
+                v2.voted = True
+
+                #Redirects the user to the voting page to vote!
+                voteNow()
+
+            #Joe103
+            elif voterID == validVoters_name[2]:
+                #Then we set Joe's voted value to True (Bob has voted)
+                v3.voted = True
+
+                #Redirects the user to the voting page to vote!
+                voteNow()
+
+def checkPassword(password):
+    #Password stored in the registration system
+    passcode = "VoteNow"
+
+    #If the password input by the user does not match the stored passcode
+    if password != passcode:
+        #Then we display a message that the password does not match the passcode and redirect the user to the initial page menu.
+        print("You have entered an incorrect password. Please return to the main page!")
+
+        global layout, Column
+        layout = 3
+        window[f'-COL{layout}-'].update(visible=False)
+        layout = layout - 2
+        window[f'-COL{layout}-'].update(visible=True)
+
+    #If the password input by the user does match the stored passcode
+    else:
+        displayVoteCount()
+
+#def displayVoteCount():
+
+
+##-----MAIN EVENT LOOP---------------------------------##
+layout = 1  # The currently visible layout
+while True:
+    event, values = window.read()
+    if event in (None, 'Exit Program'):
+        break
+    if event == "Login to Cast Vote":
+        window[f'-COL{layout}-'].update(visible=False)
+        #layout = layout + 1 if layout < 3 else 1
+        layout = layout + 1
+        window[f'-COL{layout}-'].update(visible=True)
+    elif event == "Enter Voter ID":
+        checkVoterID(values[0])
+    elif event == "Main Menu":
+        window[f'-COL{layout}-'].update(visible=False)
+        layout = layout - 1
+        window[f'-COL{layout}-'].update(visible=True)
+    elif event == "See Voting Results":
+        window[f'-COL{layout}-'].update(visible=False)
+        layout = layout + 2
+        window[f'-COL{layout}-'].update(visible=True)
+    elif event == "Enter Password":
+        checkPassword(values[1])
+    elif event == "Back":
+        window[f'-COL{layout}-'].update(visible=False)
+        layout = layout - 2
+        window[f'-COL{layout}-'].update(visible=True)
+window.close()
